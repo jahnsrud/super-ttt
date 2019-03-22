dd9e71fa8f84d9fa4241f01a82431ea8136578fe
Markus Jahnsrud <markus@soriko.no>
2019-03-22 20:53:57 +0100

    Here we go: Fragment config

---
+++ TicTacToe/app/src/main/java/no/jahnsrud/tictactoe/MainFragment.kt
@@ -0,0 +1,38 @@
+package no.jahnsrud.tictactoe
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_main, container, false)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playButton.setOnClickListener({
+            Navigation.findNavController(this.view!!).navigate(R.id.gameFragment2)
+
+        })
+    }
+
+
+
+}
