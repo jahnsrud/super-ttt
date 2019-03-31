f208c42d7b459cb575a15b229ed35e08c06db940
Markus Jahnsrud <markus@soriko.no>
2019-03-31 20:44:24 +0200

    Structural improvements :)

---
+++ TicTacToe/app/src/main/java/no/jahnsrud/tictactoe/Models/Player.kt
@@ -0,0 +1,6 @@
+package no.jahnsrud.tictactoe.Models
+
+open class Player (
+    var name:String,
+    var numberOfWins:Int
+)
--- TicTacToe/app/src/main/java/no/jahnsrud/tictactoe/StartFragment.kt
+++ TicTacToe/app/src/main/java/no/jahnsrud/tictactoe/StartFragment.kt
@@ -1,6 +1,7 @@
 package no.jahnsrud.tictactoe
 
-
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -9,28 +10,31 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_main.*
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class MainFragment : Fragment() {
+
+class StartFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_main, container, false)
-
+        return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
     override fun onStart() {
         super.onStart()
+
         playButton.setOnClickListener({
-            Navigation.findNavController(this.view!!).navigate(R.id.startFragment)
+            Navigation.findNavController(this.view!!).navigate(R.id.gameFragment)
 
         })
+
     }
 
 
