66948afbdf934588a059339ca95c38554cb9cd66
Markus Jahnsrud <markus@soriko.no>
2019-04-01 20:09:27 +0200

    It's beginning to look a lot like Tic Tac Toe

--- TicTacToe/app/src/main/java/no/jahnsrud/tictactoe/GameFragment.kt
+++ TicTacToe/app/src/main/java/no/jahnsrud/tictactoe/GameFragment.kt
@@ -6,18 +6,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_game.*
 
+class GameFragment : View.OnClickListener, Fragment(){
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
-class GameFragment : Fragment() {
+    var player1Moves = intArrayOf()
+    var player2Moves = intArrayOf()
+    var activePlayer = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +22,51 @@ class GameFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        resetButton.setOnClickListener({
+            reset()
+        })
+
+        onClick(button1)
+    }
+
+    override fun onClick(v: View?) {
+
+        print("ID: " + v?.tag)
+
+        when (v?.id) {
+
+
+        }
+
+    }
+
+    fun didMakeMove(view: View) {
+        print("HEI")
+    }
+
+    fun reset() {
+        Toast.makeText(activity, "Kommer snart", Toast.LENGTH_SHORT).show()
+    }
+
+    /*
+
+    Mulige vinnerkombinasjoner
+
+    1, 2, 3
+    1, 4, 7
+    1, 5, 9
+    3, 6, 9
+    4, 5, 6
+    7, 5, 3
+    7, 8, 9
+
+     */
+
+
 
 
 }
